e37204b92c522e742421cf4409a83bf546047c87
School Practical Project <152527120+sakibansari546@users.noreply.github.com>
2024-03-12 22:45:19 +0530

    add style for responsive

--- routes/listing.js
+++ routes/listing.js
@@ -1,81 +1,61 @@
 const express = require('express');
 const router = express.Router();
 
-const wrapAsync = require("../utils/wrapAsync.js")
+const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 
 const { listingSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner } = require('../middleware.js');
 
-const User = require("../models/user.js")
+const User = require("../models/user.js");
 
 const listingController = require("../controller/listings.js");
 
-const multer = require('multer')
-const { storage } = require("../cloudConfig.js")
-const upload = multer({ storage })
-
+const multer = require('multer');
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 // Validate Schema Middleware
 const validateListing = (req, res, next) => {
-    let { error } = listingSchema.validate(req.body)
-    // console.log(result);
+    let { error } = listingSchema.validate(req.body);
     if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",")
-        throw new ExpressError(404, errMsg)
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(404, errMsg);
     } else {
         next();
     }
-}
+};
+
 // Index Route
 router.get('/', wrapAsync(listingController.index));
 
-router.get('/search', async (req, res) => {
-    try {
-        const { country } = req.query;
-        let listings;
-        if (!country) {
-            listings = await Listing.find().populate("owner");
+// Search Route
+router.get('/search', wrapAsync(async (req, res) => {
+    const { country } = req.query;
+    let listings;
 
-        } else {
-            listings = await Listing.find({ country }).populate("owner");
-        }
-        res.render("./listings/index.ejs", { listings });
-    } catch (error) {
-        console.error('Error searching listings:', error);
-        res.status(500).send('Internal Server Error');
+    if (!country) {
+        listings = await Listing.find().populate("owner");
+    } else {
+        listings = await Listing.find({ country }).populate("owner");
     }
-});
 
-
-
-
+    res.render("./listings/index.ejs", { listings });
+}));
 
 // Create Route
-router.get("/new", isLoggedIn, listingController.renderNewForm)
-
+router.get("/new", isLoggedIn, listingController.renderNewForm);
 router.post("/", isLoggedIn, upload.single('listing[image]'), wrapAsync(listingController.createListing));
-
-
-
 
 // Show Route
 router.get('/:id', wrapAsync(listingController.showListing));
 
-
-
-
 // Edit Route
 router.get('/:id/edit', isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
-
-// update Route
 router.put("/:id", isLoggedIn, isOwner, upload.single('listing[image]'), validateListing, wrapAsync(listingController.editListing));
 
-
-// Delte Route
-router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingController.deleteListing))
-
-
+// Delete Route
+router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
 
 module.exports = router;
